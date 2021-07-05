bef9596c3bd4a48ce6bc28116c1dafed253b3f86
Michael Voigt <Michael@MichaelVoigt.de>
2021-07-05 14:09:24 +0200

    Commit 12

--- src/main/java/com/MichaelVoigt/app/App.java
+++ src/main/java/com/MichaelVoigt/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "I'm the Light Barrier Controller controlling the Light Barrier!" );
+        System.out.println( "I'm the Light Barrier Controller controlling the Light Barrier." );
     }
 }
