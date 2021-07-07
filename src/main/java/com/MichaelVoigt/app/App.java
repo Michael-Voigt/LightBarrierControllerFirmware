9e1dd9163a03630896108adb894f212fec7e9e2b
Michael Voigt <Michael@MichaelVoigt.de>
2021-07-07 10:58:34 +0200

    Commit 22

--- src/main/java/com/MichaelVoigt/app/App.java
+++ src/main/java/com/MichaelVoigt/app/App.java
@@ -8,6 +8,12 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "I'm the Light Barrier Controller controlling the Light Barrier." );
+        System.out.println( "Light Barrier Controller - Version 1.0" );
+        System.out.println( "Searching for Light Barriers..." );
+        System.out.println( "4 Light Barriers found." );
+        System.out.println( "Connecting to Light Barriers..." );
+        System.out.println( "Done." );
+        System.out.println( "" );
+        System.out.println( "I'm the Light Barrier Controller controlling now 4 Light Barriers." );
     }
 }
