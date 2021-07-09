a97025260328a36eb57a2894432e82e0d1cf1313
Michael Voigt <Michael@MichaelVoigt.de>
2021-07-09 14:46:55 +0200

    new class LightBarrierController

--- src/main/java/com/MichaelVoigt/app/App.java
+++ src/main/java/com/MichaelVoigt/app/App.java
@@ -1,5 +1,7 @@
 package com.MichaelVoigt.app;
 
+import com.MichaelVoigt.LightBarrierController;
+
 /**
  * Hello world!
  *
@@ -9,11 +11,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Light Barrier Controller - Version 1.0 Dev Branch " );
-        System.out.println( "Searching for Light Barriers..." );
-        System.out.println( "4 Light Barriers found." );
-        System.out.println( "Connecting to Light Barriers..." );
-        System.out.println( "Done." );
-        System.out.println( "" );
-        System.out.println( "I'm the Light Barrier Controller controlling now 4 Light Barriers." );
+        new LightBarrierController();
     }
 }
